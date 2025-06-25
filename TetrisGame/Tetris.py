8274272a001c8e987a9b8efb1d419f69a314b501
Sairam Yadla <sairamnarasimhareddy@gmail.com>
2025-06-25 16:56:09 +0200

    Blocks

--- TetrisGame/Tetris.py
+++ TetrisGame/Tetris.py
@@ -1,11 +1,45 @@
 import pygame
 
+#blocks are the various shapes when occupied in a Matrix. Here it is a 3x3 Matrix
+blocks = [
+    [[1,4,7], [3, 4, 5]], #straight
+    [[1,3,4,5,7]], #cross
+    [[0,1,4,5], [1,3,4,6]], #two on two 1
+    [[1,2,3,4],[0,3,4,7]], #two on two
+    [[0,1,3,6],[0,1,2,5],[2,5,7,8],[3,6,7,8]], #L1
+    [[1,2,5,8], [5,6,7,8], [0,3,6,7],[0,1,2,8]], #one one three
+    [[4,6,7,8],[0,3,4,6],[0,1,2,4],[2,4,5,8]] #one on three
+]
+
+
+class Block:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        self.type = 0
+        self.rotation = 0
+
+    def shape(self):
+        return blocks[self.type][self.rotation]
+
+
+def draw_block():
+    for y in range(3):
+        for x in range(3):
+            if y * 3 + x in block.shape():
+                pygame.draw.rect(screen,
+                                 (255, 255, 255),
+                                 [(x + block.x) * grid_size + x_gap + 1,
+                                  (y + block.y) * grid_size + y_gap + 1,
+                                 grid_size -2 , grid_size - 2]
+                                 )
 
 
 pygame.init()
 screen = pygame.display.set_mode((800, 800))
 pygame.display.set_caption('Tetris Game')
 game_over = False
+block = Block(5, 6)
 
 #variables for drawing grid
 grid_size = 30
@@ -28,6 +62,7 @@ while not game_over:
 
     screen.fill((0, 0, 0))
     draw_grid(rows, cols,grid_size,  x_gap, y_gap)
+    draw_block()
     pygame.display.update()
 
 pygame.quit()
