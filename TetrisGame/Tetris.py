45ba5450f257577b56bda148bc2db9ce750faf16
Sairam Yadla <sairamnarasimhareddy@gmail.com>
2025-06-25 17:46:47 +0200

    Side move for blocks

--- TetrisGame/Tetris.py
+++ TetrisGame/Tetris.py
@@ -48,22 +48,62 @@ rows = screen.get_height() // grid_size
 x_gap = (screen.get_width() - cols * grid_size) // 2
 y_gap = (screen.get_height() - rows * grid_size) // 2
 
+clock = pygame.time.Clock()
+fps = 2
+
+
 
 def draw_grid(cols, rows, grid_size, x_gap, y_gap):
     for y in range(rows): #rows
         for x in range(cols): #columns
             pygame.draw.rect(screen,(100,100,100), [x * grid_size + x_gap,y * grid_size + y_gap,grid_size,grid_size], 1)
 
+def drop_block():
+    can_drop = True
+    for y in range(3):
+        for x in range(3):
+            if y * 3 + x in block.shape():
+                if block.y + y >= rows - 1:
+                    can_drop = False
+    if can_drop:
+        block.y += 1
+
+def side_move(dx):
+    can_move = True
+    for y in range(3):
+        for x in range(3):
+            if y * 3 + x in block.shape():
+                if x + block.x >= cols - 1 and dx == 1:
+                    can_move = False
+                elif x + block.x < 1 and dx == -1:
+                    can_move = False
+    if can_move:
+        block.x += dx
+
+
 
 while not game_over:
+    clock.tick(fps)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
 
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_LEFT:
+            side_move(-1)
+        if event.key == pygame.K_RIGHT:
+            side_move(1)
+
+
     screen.fill((0, 0, 0))
     draw_grid(rows, cols,grid_size,  x_gap, y_gap)
     draw_block()
+    if event.type != pygame.KEYDOWN:
+        drop_block()
+
     pygame.display.update()
+
+
 
 pygame.quit()
 
